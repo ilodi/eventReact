4f439f72acb0dd1fb2dc9c252de78641f95e31e2
David Lopez <lodi@MacBook-Pro-de-David.local>
2019-07-02 00:13:28 -0500

    axios

--- eventosreact/src/App.js
+++ eventosreact/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from './components/Header'
-
+import CategoriasProvider from './context/CategoriasContext'
 function App() {
   return (
-    <>
+    <CategoriasProvider>
       <Header />
-    </>
+    </CategoriasProvider>
   );
 }
 
---
+++ eventosreact/src/context/CategoriasContext.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+//Crear context Provider
+const CategoriasContext = React.createContext();
+
+//Crear consumer
+export const CategoriasConsumer = CategoriasContext.Consumer;
+
+class CategoriasProvider extends Component {
+
+  token = 'KPEUQDTDUOI4YVKOQLCQ';
+
+  state = {
+    categorias: ''
+  }
+
+  componentDidMount() {
+    this.obtenerCategorias();
+  }
+  //Crear metodo para componen
+  obtenerCategorias = async () => {
+    //Axios consumir API
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+    let categorias = await axios.get(url);
+  
+    //colocar respuesta en el state 
+    this.setState({
+      categorias : categorias.data.categories
+    })
+  }
+
+  render() {
+    return (
+      <CategoriasContext.Provider
+        value={{
+          categorias: this.state.categorias
+        }}
+      >
+        {/* //Para que funcione debes de agregar */}
+        {this.props.children}
+      </CategoriasContext.Provider>
+    );
+  }
+}
+
+export default CategoriasProvider; 
